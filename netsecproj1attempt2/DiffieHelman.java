b4a857005eef252f1ed1b811b29e391c5545546d
Aaron Guillen <aaronguillen@users.noreply.github.com>
2016-10-11 02:04:13 -0400

    Add files via upload

---
+++ netsecproj1attempt2/DiffieHelman.java
@@ -0,0 +1,410 @@
+package netsecproj1attempt2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Random;
+import java.math.BigInteger;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.security.SecureRandom;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.UnknownHostException;
+
+public class DiffieHelman {
+    private long p; //A large prime number
+    private long a; //This is a primitive root mod p
+    private long x; //Secret number 1 <= x <= (p - 2)
+    private long k; //The private key
+    private long o; //The other party's public key
+            
+    public DiffieHelman () { //Set small numbers that won't really work. They should be changed.
+        p = 7;
+        a = 3;
+        x = 5;
+        k = -1;
+        o = -1;
+    }
+    
+    /**
+     * Would not recommend supplying this method with a value over 18 bits in
+     * length.
+     * 
+     * @param prim A prime number, this method assumes you will provide it with
+     * a number that is prime or at least probably prime
+     * @return a primitive root modulo prim
+     */
+    private long primitiveRoot(long prim) {
+        //We assume the number we are given is prime.
+        //Then, by Euler's totient function, prim has prim - 1 relative primes
+        //We say s = eulersTotient(prim) and, assuming prim is prim, s = prim - 1
+        long s = prim - 1;
+        
+        //Next, we need to collect all the numbers that are prime and that divide s
+        //that is, we we need to know the prime factors of s
+        //Yes, this is a process and why this method should not be given too
+        //large a number
+        ArrayList<Long> primeFactors = new ArrayList<>();
+        for (long i = 2; i <= (s / 2); i ++) {
+           if ((s % i == 0) && new BigInteger(Long.valueOf(i).toString()).isProbablePrime(10)) {
+               primeFactors.add(i);
+           }
+           System.out.println((((double)i / (s / 2)) * 100) + "% complete");
+        }
+        
+        //Now, we have all the (probably) prime factors of s. We divide s by its
+        //prime factors and remember those numbers
+        ArrayList<Long> nums = new ArrayList<>();
+        for (Long item : primeFactors) {
+            nums.add(s / item);
+        }
+        
+        //Now we have what we need to find the lowest primitive root.
+        //We simply begin from x = 2 and check every number for:
+        //x ^ valuesInnums % prim
+        //If we get one, our number is not a primitive root module prim
+        //If we do not get one, we have found a primitive root
+        boolean isPrimRoot;
+        long primRoot = -1; //Error
+        for (long i = 2; i < prim; i++) {
+            isPrimRoot = true;
+            for (Long item : nums) {
+                if (expSqu(i, item, prim) == 1) {
+                    isPrimRoot = false;
+                    break;
+                }
+            }
+            if (isPrimRoot) {
+                primRoot = i;
+                break;
+            }
+        }
+        
+        //Now, we have our lowest primitive root mod prim. We can use this to
+        //find all other primitive roots mod prim, however, this is a resource
+        //intensive operation, as we have to compute primRoot ^ m % prim for all
+        //1 <= m < prim and determine whether or not the gcd of this value and s
+        //is one.
+        //Though it will (I believe) significantly weaken the strength of our
+        //key exchange, we're going to stick with using our smallest primitive
+        //root
+        
+        return primRoot;
+    }
+    
+    
+    private long generateRandomLong () {
+        long LOWER_RANGE = 0; //Assign lower range value
+        long UPPER_RANGE = 0x7FFFFFFFFFFFFFFFL; //Assign upper range value
+        Random random = new Random();
+        return (LOWER_RANGE + (long)(random.nextDouble() * (UPPER_RANGE - LOWER_RANGE))) - 1;
+    }
+    
+    /* Included because I don't really want to delete them.
+    private long generateRandomLong (long UPPER_RANGE) {
+        long LOWER_RANGE = 0; //assign lower range value
+        Random random = new Random();
+        return (LOWER_RANGE + (long)(random.nextDouble() * (UPPER_RANGE - LOWER_RANGE))) - 1;
+    }
+    
+    private long generateRandomLong (long LOWER_RANGE, long UPPER_RANGE) {
+        Random random = new Random();
+        return (LOWER_RANGE + (long)(random.nextDouble() * (UPPER_RANGE - LOWER_RANGE))) - 1;
+    }
+    */
+    
+    //Generate a large psuedo-prime number
+    private long primeMaker (int bitLength) {
+        return BigInteger.probablePrime(bitLength, new SecureRandom()).longValue();
+    }
+    
+    private boolean millerRabin (long n) {
+        //The Miller-Rabin Primality Test
+        
+        //n must be greater than 1
+        if (!(n > 1)) {
+            return false;
+        }
+        long k = 0;
+        long m = n - 1;
+        long b;
+        long a;
+        
+        //Find 2^k * d = n - 1 = m
+        while (m % 2 != 1) {
+            m = m / 2;
+            k++;
+        }
+        a = 2;
+        while (a < (n - 1)) {
+            b = (long)Math.pow(a, m) % n;
+            if ((b == 1 % n) ||( b == -1 % n)) {
+                //n is probably prime
+                return true;
+            }
+            for (int i = 1; i < (k - 1); i++) {
+                b = (long)Math.pow(b, 2) % n;
+                if (b == 1 % n) {
+                    //n is composite
+                    return false;
+                }
+                if (b == -1 % n) {
+                    //n is probably prime
+                    return true;
+                }
+            }
+            a++;
+        }
+        return false;
+    }
+    
+    //computeX() with a parameter, they sent us an x they want to use.
+    public void computeX (long x) {
+        /* x must be a number such that 1 <= x <= (p - 2)
+           So if their number is !(1 <= x <= (p - 2)), we'll force it into
+           range.
+        */
+        if ((x > (p - 2)) || (x < 1)) {
+            x = x % (p - 2);
+        }
+        this.x = x;
+    }
+    
+    //computeX() without parameter, we generate x
+    public void computeX () {
+        this.x = generateRandomLong();
+        if ((this.x > (p - 2)) || (this.x < 1)) { //Force x into range 1 <= x <= (p - 2)
+                this.x = this.x % (p - 2);
+            }
+    }
+    
+    //computeP() with a parameter, they sent us an p they want to use.
+    public void computeP (long p) {
+        this.p = p;
+        /*if (!millerRabin(p)) {
+            computeP()
+        }*/
+    }
+    
+    //computeP() without parameter, we generate p
+    public void computeP () {
+        this.p = primeMaker(18);
+    }
+      
+    //computeA() with a parameter, they sent us an a they want to use.
+    public void computeA (long a) {
+        this.a = a;
+    }
+    
+    //computeA() without parameter, we generate a
+    public void computeA() {
+        this.a = primitiveRoot(p);
+    }
+    
+    public long getMyPublicKey () {
+        return ((long)Math.pow(a, x) % p);
+    }
+    
+    public byte[] getMyPublicKeyBytes () {
+        //Stole this idea from StackOverflow.com from user Waldheinz
+        return ByteBuffer.allocate(8).putLong(getMyPublicKey()).array();
+    }
+    
+    public void publicKeyServer(int port) {
+        /*
+        //Put our public key in a text file
+        Writer w = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("output.txt"), "utf-8"));
+        w.write(String.valueOf(getMyPublicKey()));
+        w.close();
+        */
+        if ((port < 0) || (port > 0xFFFF)) {
+            port = 8080;
+        }
+        
+        ServerSocket Server = null;
+        
+        try {
+            Server = new ServerSocket(8080);
+        }
+        catch (IOException e) {
+            System.err.println("Could not listen on port " + port);
+            System.exit(-1);
+        }
+        
+        //Set to zero in case we don't get any good input
+        String clientInput = "0";
+        
+        //Accepts a connection to a socket.
+        Socket connectedSocket = null;
+        try {
+            //Program waits at this line until a connection is made.
+            connectedSocket = Server.accept();
+        }
+        catch (IOException e) {
+            System.err.println("Could not accept connection");
+            System.exit(-1);
+        }
+        System.out.println("Connection accepted on port " + port);
+        
+        BufferedReader inFromClient = null;
+        PrintWriter outToClient = null;
+        
+        try {
+            inFromClient = new BufferedReader(new InputStreamReader(connectedSocket.getInputStream()));
+            outToClient = new PrintWriter(connectedSocket.getOutputStream(), true);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("IOException in BufferedReader or PrintWriter");
+            System.exit(-1);
+        }
+
+        System.out.println("BufferedReader and PrintWriter established.");
+        
+        //Print public key in bytes
+        //System.out.print("PrintWriter writing: [");
+        //byte[] pubKey = getMyPublicKeyBytes();
+        //for (byte item : pubKey) {
+        //    System.out.print(item + " ");
+        //}
+        //System.out.println("]");
+        long pubKey = getMyPublicKey();
+        System.out.println("PrintWriter writing: " + pubKey);
+        //outToClient.println(pubKey);
+        outToClient.println(pubKey);
+        
+        try {
+        clientInput = inFromClient.readLine();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error reading from client");
+            System.exit(-1);
+        }
+        
+        //RIGHT HERE IS WHERE THE PROBLEMS BEGIN.
+        
+        System.out.printf("BufferedReader read: %s\n\n", clientInput);
+        
+        //System.out.println(Math.pow(Long.valueOf(clientInput).longValue(), x));
+        //o = (long)(Math.pow(Long.valueOf(clientInput).longValue(), x) % p);
+        
+        o = expSqu(Long.valueOf(clientInput).longValue(), x, p);
+        
+        try {
+            Server.close();
+            connectedSocket.close();
+        }
+        catch (IOException e) {
+            System.err.println("Error cleaning up");
+            e.printStackTrace();
+        }
+    }
+    
+    public void publicKeyClient(String ip, int port) {
+        if ((port < 0) || (port > 0xFFFF)) {
+            port = 8080;
+        }
+        
+        Socket socket = null;
+        
+        try {
+            socket = new Socket(ip, port);
+        }
+        catch (UnknownHostException e) {
+            System.err.println("Unable to connect to " + ip + " at " + port);
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        catch (IOException e) {
+            System.err.println("IOExcpetion");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        
+        System.out.println("Connection established to " + ip + " at " + port);
+        
+        BufferedReader incoming = null;
+        try {
+            incoming = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        }
+        catch (IOException e) {
+            System.err.println("Problem creating BufferedReader");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        
+        System.out.println("BufferedReader established");
+        
+        long pubKey = getMyPublicKey();
+        try {
+            //socket.getOutputStream().write(pubKey);
+            new PrintWriter(socket.getOutputStream(), true).println(pubKey);
+        }
+        catch (IOException e) {
+            System.err.println("Unable to write to socket");
+            e.printStackTrace();
+            System.exit(-1);
+        }        
+        
+        System.out.println("Write successful. Wrote: " + pubKey);
+        
+        String lineIn = null;
+        try {
+            lineIn = incoming.readLine();
+        }
+        catch (IOException e) {
+            System.err.println("Unable to read from socket");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        
+        long fromServer = Long.valueOf(lineIn).longValue();
+        
+        System.out.println("Read from socket: " + fromServer);
+        
+        o = expSqu(Long.valueOf(fromServer).longValue(), x, p);
+        
+        try {
+            socket.close();
+        }
+        catch (IOException e) {
+            System.err.println("Problem closing socket");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+    
+    /*
+    public long getPrivateKey() {
+        return (long)Math.pow(o, x);
+    }
+    */
+    
+    public long getPrivateKey() {
+        return o;
+    }
+    
+    //Stolen from http://stackoverflow.com/questions/30694842/exponentiation-by-squaring
+    //Exponentiation by Squaring
+    public long expSqu(long base, long exp, long mod) {
+        if ((base < 1) || (exp < 0) || (mod < 1)) {
+            System.out.println("Base: " + base);
+            System.out.println("Exponent: " + exp);
+            System.out.println("Modulus: " + mod);
+            return -1;
+        }
+        
+        long result = 1;
+        while (exp > 0) {
+            if ((exp % 2) == 1) {
+                result = (result * base) % mod;
+            }
+            base = (base * base) % mod;
+            exp = exp / 2;
+        }
+        return result;
+    }
+}
---
+++ netsecproj1attempt2/NetSecProj1Attempt2.java
@@ -0,0 +1,39 @@
+package netsecproj1attempt2;
+
+import java.util.Scanner;
+
+public class NetSecProj1Attempt2 {
+    
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+        int t; 
+        DiffieHelman d = new DiffieHelman();
+        
+        
+        
+        do {
+            System.out.print("Enter\n1:\tTo be Client\n2:\tTo be Server\n3:\tExit\n::->  ");
+            t = input.nextInt();
+            System.out.println();
+            switch(t) {
+                case 1:
+                    d.publicKeyClient("localhost", 8080);
+                    break;
+                case 2:
+                    System.out.println("Calculating your public key ...");
+                    d.computeP();
+                    d.computeA();
+                    d.publicKeyServer(8080);
+                    break;
+                case 3:
+                    System.exit(0);
+                default:
+            }
+        } while ((t != 1) && (t != 2));
+        
+        
+        System.out.printf("Your public key:\t%d =||= 0x%X\n", d.getMyPublicKey(), d.getMyPublicKey());
+        System.out.printf("Your private key:\t%d =||= 0X%x\n", d.getPrivateKey(), d.getPrivateKey());
+    }
+}
